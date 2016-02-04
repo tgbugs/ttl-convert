e988875f56b753e93a8ebb85e42af51141c0f5b1
Tom Gillespie <tgbugs@gmail.com>
2016-02-03 18:52:32 -0800

    updated to use owlapi 4.1.4 and added a check to make sure prefixes were supported before attempting to caste

--- src/main/java/scicrunch/App.java
+++ src/main/java/scicrunch/App.java
@@ -14,6 +14,7 @@ import java.util.*;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.formats.TurtleDocumentFormat;
 import org.semanticweb.owlapi.formats.PrefixDocumentFormat;
+import org.semanticweb.owlapi.formats.OBODocumentFormat;
 import org.semanticweb.owlapi.model.*; // ontology manager lives here
 
 import org.semanticweb.owlapi.io.FileDocumentSource;
@@ -34,10 +35,13 @@ public class App
         //ontology.getImports();
 
         OWLDocumentFormat owlformat = manager.getOntologyFormat(ontology);
-        PrefixDocumentFormat newPrefixes = (PrefixDocumentFormat) owlformat;
 
         TurtleDocumentFormat format = new TurtleDocumentFormat();
-        format.copyPrefixesFrom(newPrefixes);
+
+        if (owlformat.isPrefixOWLOntologyFormat() == true) {  // obo does not support prefixes
+            PrefixDocumentFormat newPrefixes = (PrefixDocumentFormat) owlformat;
+            format.copyPrefixesFrom(newPrefixes);
+        }
         //manager.setOntologyFormat(ontology, format);
         //BUG hasDbXref structure fails to translate using owlapi 4.1.1 but works on 4.0.2
         manager.saveOntology(ontology, format, new FileDocumentTarget(outfile));
